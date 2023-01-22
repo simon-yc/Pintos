8afa831b15847adc36ea4cdcb8d709d97a2a42a0
LindaS46 <zhonghuis2020@gmail.com>
2023-01-22 17:15:28 -0500

    alarm clock part 1
    code implementation

--- src/threads/thread.h
+++ src/threads/thread.h
@@ -89,6 +89,7 @@ struct thread
     uint8_t *stack;                     /* Saved stack pointer. */
     int priority;                       /* Priority. */
     struct list_elem allelem;           /* List element for all threads list. */
+    int64_t sleep_remaining;             /* Thread remaining sleep time. */
 
     /* Shared between thread.c and synch.c. */
     struct list_elem elem;              /* List element. */
@@ -129,6 +130,7 @@ void thread_yield (void);
 /* Performs some operation on thread t, given auxiliary data AUX. */
 typedef void thread_action_func (struct thread *t, void *aux);
 void thread_foreach (thread_action_func *, void *);
+void thread_update_remaining_sleep(struct thread *, void *);
 
 int thread_get_priority (void);
 void thread_set_priority (int);
