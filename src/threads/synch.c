611dd407123bdcbc74d9b0cf19a82fda4c780b3e
MmxMflora <fxie@estateably.com>
2023-02-04 22:41:55 -0500

    Update design doc

--- src/threads/synch.c
+++ src/threads/synch.c
@@ -188,6 +188,7 @@ lock_init (struct lock *lock)
 
   lock->holder = NULL;
   sema_init (&lock->semaphore, 1);
+  lock->max_priority = 0;
 }
 
 /* Acquires LOCK, sleeping until it becomes available if
