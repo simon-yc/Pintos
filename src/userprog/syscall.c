96a17657eb46f7702c355ae15773c3813f9d11e9
LindaS46 <87672649+LindaS46@users.noreply.github.com>
2023-02-22 16:25:08 -0500

    Update syscall.c

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -5,6 +5,7 @@
 #include "threads/thread.h"
 #include "userprog/pagedir.h"
 #include "threads/vaddr.h"
+#include "userprog/process.h"
 
 static void syscall_handler (struct intr_frame *);
 
@@ -39,28 +40,29 @@ put_user (uint8_t *udst, uint8_t byte)
   return error_code != -1;
 }
 
-static void
+static bool
+valid_check (const void *usrc_)
+{
+  if (is_kernel_vaddr (usrc_))
+    return false;
+  if (pagedir_get_page (thread_current ()->pagedir, usrc_) == NULL)
+   	return false;
+	return true;
+}
+
+static bool
 copy_in (void *dst_, const void *usrc_, size_t size)
 {
+  // error checking if address is valid
+  if (! valid_check (usrc_))
+    return false;
+
   uint8_t *dst = dst_;
   const uint8_t *usrc = usrc_;
 
   for (; size > 0; size--, dst++, usrc++)
     *dst = get_user (usrc);
-
-  // TODO: error checking if address is valid
-}
-
-static bool
-valid_check (struct intr_frame *f UNUSED)
-{
-  if (f == NULL)
-    return false;
-  if (is_kernel_vaddr (f->esp))
-    return false;
-  if (pagedir_get_page (thread_current ()->pagedir, f->esp) == NULL)
-   	return false;
-	return true;
+  return true;
 }
 
 static void
@@ -70,71 +72,74 @@ handle_umimplement (int syscall_number)
 }
 
 static void
+handle_exit (struct intr_frame *f)
+{
+  thread_current ()->exit_code = *(((int *) f->esp) + 1);
+  thread_exit ();
+}
+static void
 syscall_handler (struct intr_frame *f UNUSED) 
 {
   unsigned syscall_number;
-  int args[3];
-  // check if valid address
-  if (! valid_check (f))
-    {
-      thread_current ()->exit_code = *(((int *) f->esp) + 1);
-      thread_exit ();
-    }
+  int args[3];  
   //extract the syscall number
-  copy_in(&syscall_number, f->esp, sizeof syscall_number);
+  if (!copy_in(&syscall_number, f->esp, sizeof syscall_number))
+    handle_exit (f);
   // printf("  ***syscall number: %u (should be %u)\n", syscall_number, SYS_WRITE);
 
   switch (syscall_number) {
-  	case 0:
+  	case 0: // halt
       {
         shutdown_power_off (syscall_number);
         break;
       }
-    case 1:
+    case 1: // exit
       {
-        thread_current ()->exit_code = *(((int *) f->esp) + 1);
-        thread_exit ();
+        handle_exit (f);
         break;
       }
-    case 2:
-      {
-        handle_umimplement (syscall_number);
-        break;
-      }
-    case 3:
+    case 2: // exec
       {
         handle_umimplement (syscall_number);
         break;
       }
-    case 4:
+    case 3: // wait
+      {
+        if (!copy_in(args, (uint32_t *) f->esp + 1, sizeof *args * 1))
+          handle_exit (f);
+  	    f->eax = (uint32_t) process_wait (args[2]);
+        break;
+      }
+    case 4: // create
       {
         handle_umimplement (syscall_number);
         break;
       }
-    case 5:
+    case 5: // remove
       {
         handle_umimplement (syscall_number);
         break;
       }
-    case 6:
+    case 6: // open
       {
         handle_umimplement (syscall_number);
         break;
       }
-    case 7:
+    case 7: // filesize
       {
         handle_umimplement (syscall_number);
         break;
       }
-    case 8:
+    case 8: // read
       {
         handle_umimplement (syscall_number);
         break;
       }
-    case 9:
+    case 9: // write
       {
         //extract the 3 arguements
-        copy_in(args, (uint32_t *) f->esp + 1, sizeof *args * 3);
+        if (!copy_in(args, (uint32_t *) f->esp + 1, sizeof *args * 3))
+          handle_exit (f);
         // printf("  ***fd: %d (should be %u)\n", args[0], STDOUT_FILENO);
         // printf("  ***buffer address: %p\n", args[1]);
         // printf("  ***size: %u\n", args[2]);
@@ -146,18 +151,18 @@ syscall_handler (struct intr_frame *f UNUSED)
         f->eax = args[2];
         break;
       }
-    case 10:
+    case 10:  // seek
       {
         handle_umimplement (syscall_number);
         break;
 
       }
-    case 11:
+    case 11:  // tell
       {
         handle_umimplement (syscall_number);
         break;
       }
-    case 12:
+    case 12:  // close
       {
         handle_umimplement (syscall_number);
         break;
